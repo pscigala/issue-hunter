a9620752ff06a087541666ef70c534ccb8643d04
pscigala <przemyslaw.scigala@gmail.com>
2020-04-15 23:51:59 +0100

    auth support search list support formatting

--- issueHunt.py
+++ issueHunt.py
@@ -1,18 +1,20 @@
 import requests
 
 github_root_url = 'http://api.github.com'
+github_token = ''
+github_username = ''
 
 
 def get_issue(repo, labels):
     endpoint = f'/repos/{repo}/issues'
     params = {'labels': labels, 'state': 'open'}
     url = github_root_url + endpoint
-    r = requests.get(url, params=params)
+    r = requests.get(url, params=params, auth=(github_username, github_token))
     return r.json()
 
 
 def print_head(repo, labels):
-    header = f'#repo: {repo} #labels:{labels}'
+    header = f'🤗 #repo: {repo} #labels:{labels}'
     bar = "=" * (len(header) + 3)
     print(bar)
     print(header)
@@ -20,15 +22,19 @@ def print_head(repo, labels):
 
 
 def print_issue(issue):
-    has_pull_request=f'{("pull_requests" in issue)}'[0]
-    print(f' C:{issue["comments"]}/PR:{has_pull_request} 🤗 #{issue["number"]} {issue["title"]}')
+    has_pull_request = f'{("pull_requests" in issue)}'[0]
+    print(f'#{str(issue["number"]).ljust(6, " ")}| {issue["title"]} | C:{issue["comments"]}/PR:{has_pull_request}')
 
 
-repo = 'netlify/netlify-cms'
-labels = {'area: api', 'area: api'}
+search_list = [
+    {'repo': 'netlify/netlify-cms', 'labels': {'area: api', 'area: api'}},
+    {'repo': 'netlify/netlify-cms', 'labels': {'area: api', 'area: api'}},
+]
 
-issues = get_issue('netlify/netlify-cms', labels)
-
-print_head(repo, labels)
-for issue in issues:
-    print_issue(issue)
+for i, repo_to_check in enumerate(search_list):
+    repo = repo_to_check['repo']
+    labels = repo_to_check['labels']
+    print_head(repo, labels)
+    issues = get_issue(repo, labels)
+    for issue in issues:
+        print_issue(issue)
