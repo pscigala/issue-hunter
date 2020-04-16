dc2bd8ebbb2b92a37c9f2525e36e903087fbecb6
pscigala <przemyslaw.scigala@gmail.com>
2020-04-16 22:32:30 +0100

    added support for auth credentials from sys env

--- issueHunt.py
+++ issueHunt.py
@@ -1,15 +1,31 @@
+import os
+
 import requests
 
-github_root_url = 'http://api.github.com'
-github_token = ''
-github_username = ''
+
+def get_github_user():
+    try:
+        return os.environ["ISSUE_HUNT_GITHUB_USER"]
+    except KeyError:
+        pass
+
+
+def get_github_token():
+    try:
+        return os.environ["ISSUE_HUNT_GITHUB_TOKEN"]
+    except KeyError:
+        pass
 
 
 def get_issue(repo, labels):
     endpoint = f'/repos/{repo}/issues'
     params = {'labels': labels, 'state': 'open'}
     url = github_root_url + endpoint
-    r = requests.get(url, params=params, auth=(github_username, github_token))
+
+    if (useAuth):
+        r = requests.get(url, params=params, auth=(user, token))
+    else:
+        r = requests.get(url, params=params)
     return r.json()
 
 
@@ -26,10 +42,21 @@ def print_issue(issue):
     print(f'#{str(issue["number"]).ljust(6, " ")}| {issue["title"]} | C:{issue["comments"]}/PR:{has_pull_request}')
 
 
+github_root_url = 'http://api.github.com'
+
+user = get_github_user()
+token = get_github_token()
+useAuth = (user and token)
+
 search_list = [
     {'repo': 'netlify/netlify-cms', 'labels': {'area: api', 'area: api'}},
     {'repo': 'netlify/netlify-cms', 'labels': {'area: api', 'area: api'}},
 ]
+
+if useAuth:
+    print(f'Using auth token with username: {user}')
+else:
+    print("No auth mode")
 
 for i, repo_to_check in enumerate(search_list):
     repo = repo_to_check['repo']
